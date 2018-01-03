44dfe6557ab2a8dd18868673f406b6f03c673fce
John Daise <jdaise@gmail.com>
2018-01-03 01:38:33 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.count > 2
     array = ["dogs", "cats", "mice"]
-    array[-1]<<"and"
-    array.join(", ")
+    a = "and "<< array[-1]
+    (array.join(", ")<< a).chomp(",")
   end
 end
