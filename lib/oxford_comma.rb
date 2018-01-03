5bdd36dd19e526a886809dafa82daf5cb21bd998
John Daise <jdaise@gmail.com>
2018-01-03 01:58:55 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.count == 2
     array.join(" and ")
   elsif array.count > 2
-    a = " and "<< array[-1]
+    a = ",and "<< array[-1]
     array.pop
     array.join(", ")<< a
   end
